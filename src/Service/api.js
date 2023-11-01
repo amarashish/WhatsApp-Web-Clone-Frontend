6f1ffbaf6b12876fcec59f6da575cf8ec7af51da
“[Amar <[amarashish000@gmail.com]>
2023-11-01 20:03:55 +0530

    code refractor

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import { BASE_URL } from "../Constants/constants.js";
 
-
 export const addUser = async (data)=>{
     try{
         return await axios.post(`${BASE_URL}/add`, data);
-
     }catch(error){
-        console.log("Error while calling addUser API");
+        console.log("Error while calling addUser API", error.message);
     }
 };
 
@@ -17,7 +15,7 @@ export const addVerifier = async (data)=>{
         console.log(response.data);///////
 
     }catch(error){
-        console.log("Error while calling addVerifier API");
+        console.log("Error while calling addVerifier API", error.message);
     }
 }
 
@@ -27,7 +25,7 @@ export const getVerifier = async (data) => {
         console.log(response.data);///////
         return response.data;
     }catch(error){
-        console.log("Error while calling getVerifier API");
+        console.log("Error while calling getVerifier API", error.message);
 
     }
 }
@@ -35,8 +33,8 @@ export const getVerifier = async (data) => {
 export const updateUser = async (data)=>{
     try{
         await axios.put(`${BASE_URL}/update`, data);
-    }catch(err){
-        console.log("Error while calling updateUser API");
+    }catch(error){
+        console.log("Error while calling updateUser API", error.message);
     }
 }
 
@@ -44,24 +42,24 @@ export const getUsers = async ()=>{
     try{
         const response = await axios.get(`${BASE_URL}/users`);
         return response.data;
-    }catch(err){
-        console.log("Error while calling getUser API");
+    }catch(error){
+        console.log("Error while calling getUser API", error.message);
     }
 }
 
 export const setConversation = async (data)=>{
     try{
         await axios.post(`${BASE_URL}/conversation/add`, data);
-    }catch(err){
-        console.log("Error while calling setConversation api", err.message);
+    }catch(error){
+        console.log("Error while calling setConversation api", error.message);
     }
 }
 export const getConversation = async (data)=>{
     try{
         const response = await axios.post(`${BASE_URL}/conversation/get`, data);
         return response.data;
-    }catch(err){
-        console.log("Error while calling getConversation api", err.message);
+    }catch(error){
+        console.log("Error while calling getConversation api", error.message);
     }
 };
 
@@ -69,8 +67,8 @@ export const getAllConversation = async (id)=>{
     try{
         const response = await axios.get(`${BASE_URL}/conversations/getAll/${id}`);
         return response.data;
-    }catch(err){
-        console.log("Error while calling getAllConversation api", err.message);
+    }catch(error){
+        console.log("Error while calling getAllConversation api", error.message);
 
     }
 }
@@ -78,8 +76,8 @@ export const getAllConversation = async (id)=>{
 export const newMessage = async (data) =>{
     try{
         await axios.post(`${BASE_URL}/message/add`, data);
-    }catch(err){
-        console.log("Error While calling newMessage API", err.message);
+    }catch(error){
+        console.log("Error While calling newMessage API", error.message);
     }
 };
 
@@ -88,33 +86,23 @@ export const getAllMessages = async (id)=>{
         const response = await axios.get(`${BASE_URL}/messages/getAll/${id}`);
         return response.data;
         
-    }catch(err){
-        console.log("Error While calling getMessages API", err.message);
+    }catch(error){
+        console.log("Error While calling getMessages API", error.message);
     }
 };
 
 export const uploadFile = async (data)=>{
     try{
         return await axios.post(`${BASE_URL}/file/upload`, data);
-    }catch(err){
-        console.log("Error While calling uploadFile API", err.message);
+    }catch(error){
+        console.log("Error While calling uploadFile API", error.message);
     }
 }
 export const updateProfile = async (data)=>{
     try{
         const {sub, picture} = data;
         return await axios.post(`${BASE_URL}/updateProfile/${sub}`, picture);
-    }catch(err){
-        console.log("Error While calling updteProfile API", err.message);
+    }catch(error){
+        console.log("Error While calling updteProfile API", error.message);
     }
 }
-
-// export const getProfilePic = async (id)=>{
-//     return await axios.get(`${BASE_URL}/profile/${id}`);
-// }
-
-// export const uploadFileGCS = async(data)=>{
-//     const {sub, picture} = data;
-//     const response = await axios.post(`${BASE_URL}/updateProfile/${sub}`, picture);
-//     return response.data.data;
-// }
